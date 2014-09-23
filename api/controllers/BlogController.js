3b891063fc64809b3fa3dcedff4e543e217587ea
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2014-09-23 01:38:12 -0400

    posts

--- api/controllers/BlogController.js
+++ api/controllers/BlogController.js
@@ -9,9 +9,66 @@ module.exports = {
 	
 	index : function (request, response) {
 
-		var posts = {"posts":[{"title":"one", "body":"test"}, {"title":"two", "body":"test"}, {"title":"three", "body":"test"}]};
+		var http = require('http');
 
-  		return response.render('blog', posts);
+//get posts from the web
+		var links = ["https://raw.githubusercontent.com/NSHipster/articles/master/2012-07-07-nsindexset.md", "https://raw.githubusercontent.com/NSHipster/articles/master/2012-07-14-nscache.md", "https://raw.githubusercontent.com/NSHipster/articles/master/2012-07-24-nssortdescriptor.md"];
+		
+		generatePosts(links, function (posts) {
+			return response.render('blog', {"posts" : posts});
+		});
 	}
 };
+
+function generatePosts(links, callback) {
+
+//limit number of posts
+	var posts = [];
+	var processed = links.length;
+
+	for (i = 0; i < links.length; i++) {
+			
+		var markdown = getMarkdown(links[i], function(body) {
+
+			if (body != null) {
+				posts.push({"content" : body});
+			}
+			
+			--processed;
+
+			if (processed == 0) {
+				
+				callback(posts.sort());
+			}
+		});
+	};
+}
+
+function getMarkdown(link, callback) {
+
+	var request = require("request");
+	var marked = require('marked');
+	marked.setOptions({
+	  renderer: new marked.Renderer(),
+	  gfm: true,
+	  tables: true,
+	  breaks: false,
+	  pedantic: false,
+	  sanitize: true,
+	  smartLists: true,
+	  smartypants: false
+	});
+
+	request(link, function(error, response, body) {
+
+	  	if (error) {
+	  		callback(null);
+	  	}
+
+	  	else {
+	  		callback(marked(body));
+	  	}
+
+	});
+}
 
